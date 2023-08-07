4f9c659839237ef6d26ec53218c883f8a9ecdfd6
Valeriy <valerii.yaremenko@nure.ua>
2023-08-07 16:45:26 +0200

    forecast page update
    add full forecast information about forecasts by selected date and forecasters

--- js/getForecasters.js
+++ js/getForecasters.js
@@ -5,16 +5,61 @@ async function SetForecasters(){
     var fcJson = await getForecastersJSON();
     console.log(fcJson);
 
+    // set date input atributes - min, max, value
+    var fcDate = document.getElementById("forecast-date");
+    var date = new Date();
+    fcDate.max = date.toISOString().split("T")[0];
+    date.setDate(date.getDate() - 1);
+    fcDate.value = date.toISOString().split("T")[0];
+    date.setDate(date.getDate() - 13);
+    fcDate.min = date.toISOString().split("T")[0];
+    
     var fcList = document.getElementById("forecasters_list");
     let fcListInnerStr = "";
     fcJson.forEach((fc, ind) => {
         console.log(fc);
         
         fcListInnerStr += '<li id="fc_' + ind + '"><p class="default-paragraph">';
+        fcListInnerStr += '<input type="checkbox" id="fc_check_' + ind + '">';
         fcListInnerStr += fc.Name + ' <a href="' + fc.BaseLink +'">' + fc.BaseLink + '</a>';
         fcListInnerStr += '</p></li>';
     });
     fcList.innerHTML = fcListInnerStr;
+
+
+    document.getElementById("forecasts-link").addEventListener("click", function() {
+        let baseId = 'fc_check_';
+        let numId = 0;
+        let fullId = baseId + numId;
+        let el = document.getElementById(fullId);
+        var forecastersIds = [];
+
+        while (el != null && el != undefined){
+            let isChecked = document.getElementById(fullId).checked;
+            if (isChecked) {
+                forecastersIds.push(numId);
+            }
+
+            numId += 1;
+            fullId = baseId + numId;
+            el = document.getElementById(fullId);
+        }
+
+        let selectedDate =  document.getElementById("forecast-date").value; // 2023-08-06
+        let today = new Date().toISOString().split("T")[0];
+        let selectedDateObj = new Date(selectedDate);
+        let todayObj = new Date(today);
+        let daysAgo = Math.abs(todayObj - selectedDateObj) / 1000 / 3600 / 24; // milliseconds to days
+
+        for (let forecasterId in forecastersIds) {
+            for (let cityNum in allCities){
+                let cityId = allCities[cityNum][0];
+                let currInput = getDeviationsJSON(forecasterId, cityId, daysAgo);
+                // TODO add a tables with forecast tabs
+            }
+        }
+    });
+
 
     document.getElementById("load-block").style.display = "none";
     document.getElementById("main-block").style.display = "inline";
